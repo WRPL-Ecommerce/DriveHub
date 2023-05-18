1ff398216810b7e0478e4e530d0748ddc93bcf1e
easy <izaaz240@gmail.com>
2023-05-18 22:56:49 +0700

    refactor(server): adjust profile and brand api for better consistency

--- pages/api/brand.js
+++ pages/api/brand.js
@@ -4,13 +4,15 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     const dataToUpload = JSON.parse(req.body);
 
-    const { data, error } = await supabase
+    // insert new car brand
+    const { error } = await supabase
       .from("Brand")
       .insert([{ name: dataToUpload.name }]);
 
-    if (!error) res.status(200).json(data);
-    else res.status(500).json(error.message);
+    // return response
+    if (!error) res.status(200).json({ message: "success" });
+    else res.status(500).json(error);
   } else {
-    res.status(200).json({ message: "GET or PUT or DELETE" });
+    res.status(200).json({ message: "other method" });
   }
 }
