8d2a34254cbb411e37ed717ec23e29bec756d168
easy <izaaz240@gmail.com>
2023-05-19 20:53:24 +0700

    refactor(server): refactor profile api for better consistency
    POST method only for inserting new data, PUT method only for updating existing data. No more overlapping purpose in a single request method

--- pages/api/profile.js
+++ pages/api/profile.js
@@ -1,67 +1,57 @@
 import { supabase } from "@/lib/supabaseClient";
 
-// The http method may have been used slightly incorrectly, bear with it
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    // updating user data
-    const dataToUpload = JSON.parse(req.body);
+    // insert a new profile
+
+    let dataToUpload;
+    try {
+      dataToUpload = JSON.parse(req.body);
+    } catch (error) {
+      dataToUpload = req.body;
+    }
     console.log(dataToUpload);
 
-    // check if profile already exist
-    const { data, error } = await supabase
-      .from("Profile")
-      .select("user_id")
-      .eq("email", dataToUpload.email);
-    console.log(data);
+    // insert a new profile
+    const { error } = await supabase.from("Profile").insert([dataToUpload]);
 
-    if (data.length > 0) {
-      // profile exist
+    // return response
+    if (!error) res.status(200).json({ message: "success" });
+    else res.status(500).json(error);
+  } else if (req.method === "PUT") {
+    // updating user data
 
-      // update an existing profile
-      const { error } = await supabase
+    let dataToUpload;
+    try {
+      dataToUpload = JSON.parse(req.body);
+    } catch (error) {
+      dataToUpload = req.body;
+    }
+    console.log(dataToUpload);
+
+    if ("balance" in dataToUpload) {
+      // get the current balance
+      const { data: balance } = await supabase
         .from("Profile")
-        .update(dataToUpload)
+        .select("balance")
         .eq("email", dataToUpload.email);
 
-      if (!error) res.status(200).json({ message: "success" });
-      else res.status(500).json(error);
-    } else {
-      // profile does not exist yet
-
-      // insert a new profile
-      const { error } = await supabase.from("Profile").insert([dataToUpload]);
-
-      // return response
-      if (!error) res.status(200).json({ message: "success" });
-      else res.status(500).json(error);
+      // add the balance
+      dataToUpload.balance = String(
+        Number(dataToUpload.balance) + balance[0].balance
+      );
     }
-  } else if (req.method === "PUT") {
-    // updating user balance
 
-    const dataToUpload = JSON.parse(req.body);
-    console.log(dataToUpload);
-
-    // get the current balance
-    const { data: balance, error } = await supabase
-      .from("Profile")
-      .select("balance")
-      .eq("email", dataToUpload.email);
-
-    // add the balance
-    dataToUpload.balance = String(
-      Number(dataToUpload.balance) + balance[0].balance
-    );
-
-    // update the balance
-    const { error: err } = await supabase
+    // update the data
+    const { error } = await supabase
       .from("Profile")
       .update(dataToUpload)
       .eq("email", dataToUpload.email);
 
     // return response
-    if (!error && !err) {
+    if (!err) {
       res.status(200).json({ message: "success" });
-    } else res.status(500).json({ error1: error, error2: err });
+    } else res.status(500).json(error);
   } else {
     res.status(200).json({ message: "other method" });
   }
