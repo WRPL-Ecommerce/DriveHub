0d60467765e7b80a9fe78c15875b2b09c985238c
easy <izaaz240@gmail.com>
2023-05-20 16:37:33 +0700

    fix(server): fix incorrect response condition

--- pages/api/profile.js
+++ pages/api/profile.js
@@ -49,7 +49,7 @@ export default async function handler(req, res) {
       .eq("email", dataToUpload.email);
 
     // return response
-    if (!err) {
+    if (!error) {
       res.status(200).json({ message: "success" });
     } else res.status(500).json(error);
   } else {
