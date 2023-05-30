f79abcc0df9c7e05e828614bd4063b71ecba2d9d
easy <izaaz240@gmail.com>
2023-05-31 00:58:33 +0700

    feat(server): add an attribute sellerId for Seller model

--- models/seller/Sellers.js
+++ models/seller/Sellers.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const dbSeller = mongoose.connection.useDb("seller");
 
 const sellerSchema = new mongoose.Schema({
+  sellerId: String,
   sellerName: String,
   contactNumber: Number,
   email: String,
