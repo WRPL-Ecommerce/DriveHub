d634e097b4fc81806c9e312e2277de0ec11fb6dd
easy <izaaz240@gmail.com>
2023-06-01 12:16:39 +0700

    feat(server): add more data to individual car page

--- pages/car/[carId].js
+++ pages/car/[carId].js
@@ -1,18 +1,60 @@
 import { SellerCar } from "@/models/seller/SellerCars";
 import { Seller } from "@/models/seller/Sellers";
+import { Shipper } from "@/models/shipper/Shippers";
+import { Supplier } from "@/models/supplier/Suppliers";
 import { useUser } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/router";
 
-export default function Car({ carData, sellerData }) {
+export default function Car({
+  carData,
+  sellerData,
+  supplierData,
+  shipperList,
+}) {
   const user = useUser();
+  const { push } = useRouter();
+
+  async function handlePayment() {
+    const response = await fetch("http://localhost:3000/api/midtrans", {
+      method: "POST",
+      body: {
+
+      }
+    });
+    console.log(response);
+    const res = await response.json();
+    push(res.url);
+  }
+
   if (!user) return <h1 className=" text-3xl text-center">Login Please</h1>;
   if (!carData) return <h1 className=" text-3xl text-center">No Data</h1>;
   return (
     <div className=" mx-3">
       <p>{carData.carMake}</p>
       <p>{carData.carModel}</p>
+      <p>{carData.year}</p>
+      <p>{carData.price}</p>
+
       <p>{sellerData.sellerName}</p>
       <p>{sellerData.email}</p>
-      <button className=" bg-cyan-300 px-3 rounded-xl">Buy</button>
+      <p>{sellerData.contactNumber}</p>
+      <p>{sellerData.address}</p>
+
+      <p>{supplierData.supplierName}</p>
+      <p>{supplierData.address}</p>
+      <p>{supplierData.email}</p>
+      <p>{supplierData.contactNumber}</p>
+
+      {shipperList.map((shipper) => (
+        <p key={shipper._id}>{shipper.shipperName}</p>
+      ))}
+
+      <button
+        onClick={handlePayment}
+        className=" bg-cyan-300 hover:bg-cyan-400 px-3 rounded-xl"
+      >
+        Buy
+      </button>
     </div>
   );
 }
@@ -28,10 +70,18 @@ export async function getServerSideProps(ctx) {
   const sellerDetail = await Seller.findOne({ sellerId: carData.sellerId });
   const sellerData = JSON.parse(JSON.stringify(sellerDetail));
 
+  const supplierDetail = await Supplier.findOne({ _id: sellerData.supplier });
+  const supplierData = JSON.parse(JSON.stringify(supplierDetail));
+
+  const shipperDetail = await Shipper.find({});
+  const shipperList = JSON.parse(JSON.stringify(shipperDetail));
+
   return {
     props: {
       carData,
       sellerData,
+      supplierData,
+      shipperList,
     },
   };
 }
