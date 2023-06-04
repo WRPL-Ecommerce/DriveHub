58eaf15d730e5758db6ce3da3190681b75760bab
williamhilmysusatyo <128654764+williamhilmysusatyo@users.noreply.github.com>
2023-06-04 21:55:33 +0700

    punya william

---
+++ pages/about.js
@@ -0,0 +1,70 @@
+export default function About() {
+  return (
+    <>
+    <div className = "about">
+      <div className = "intro">
+      <div className = "meet">
+      <h1>Meet DriveHub...</h1>
+      </div>
+      <br></br>
+      <br></br>
+      <p> We consider ourself as a premier online used car marketplace connecting buyers and sellers 
+        from around the world. 
+        <br></br>
+        At <b>DriveHub</b>, we are passionate about providing a seamless and trustworthy platform for individuals to buy and sell their pre-owned vehicles.</p>
+      </div>
+
+      <br></br>
+      <br></br>
+      <div className = "history">
+      <p>Our story began in 2023 when a group of automotive enthusiasts came together 
+      with a common goal: 
+      <br></br><b><u>To revolutionize the way people buy and sell used cars. </u></b>
+      <br></br>
+      With a deep understanding of the challenges faced by both buyers and sellers, DriveHub was born.
+      </p>
+      </div>
+    
+     <br></br>
+     <br></br>
+      <div className = "historypt2">
+      Over the years, <b>DriveHub</b> has grown to become a trusted name in the industry, 
+      <br></br>
+      thanks to our commitment to transparency, security, and exceptional customer service.
+      <br></br> 
+      We have built a robust platform that offers a vast selection of high-quality used cars, 
+      <br></br>ranging from compact cars to luxury vehicles, catering to the diverse needs and budgets of our 
+      customers.
+      </div>
+
+      <br></br>
+      <br></br>
+      <div className = "historypt3">
+      At <b>DriveHub</b>, we believe in the power of technology to simplify and enhance the car-buying experience. 
+      <br></br>
+      Our advanced search filters and comprehensive vehicle history reports ensure that buyers have access to accurate information, 
+      <br></br>
+      empowering them to make informed decisions. 
+      <br></br>
+      Sellers benefit from our user-friendly listing process, reaching a wider audience of potential buyers.
+      </div>
+
+      <br></br>
+      <br></br>
+      <div className = "cta">
+      Join the <b>DriveHub</b> community today and discover the joy of finding your perfect used car or reaching a global audience of motivated buyers. 
+      <br></br>
+      Trust DriveHub to drive your car buying or selling journey forward.
+      </div>
+
+      <br></br>
+      <br></br>
+
+      </div>
+
+      <footer>
+            <p> 2023 Car Selling Website. All rights reserved.</p>
+          </footer>
+      </>
+  );
+}
